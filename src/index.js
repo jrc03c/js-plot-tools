dce94083c2bc4b99ab45241adc88ce8999a14ba1
jrc03c <jrc03c@pm.me>
2021-12-24 17:12:11 -0600

    ...

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ if (typeof module !== "undefined") {
   module.exports = {
     BrowserPlotter,
     NodePlotter,
+
+    dump() {
+      if (typeof window !== "undefined") {
+        window.plot = new BrowserPlotter()
+        window.Plotter = BrowserPlotter
+      } else {
+        global.plot = new NodePlotter()
+        global.Plotter = NodePlotter
+      }
+    },
   }
 }
 
 if (typeof window !== "undefined") {
-  window["plot"] = new BrowserPlotter()
-  window["Plotter"] = BrowserPlotter
+  window.plot = new BrowserPlotter()
+  window.Plotter = BrowserPlotter
 }
